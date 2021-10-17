24cc0f76c9e1d693ce26767dad190d74a142f2db
Łukasz Burak <25816752+bitrut94@users.noreply.github.com>
2021-10-17 20:25:29 +0200

    moved to net5.0

--- psyml.psm1
+++ psyml.psm1
@@ -1,19 +1,12 @@
-
-if ($PSEdition -eq 'Desktop') {
-    $loadPath = 'net4.8'
-} else {
-    $loadPath = 'netstandard2.1'
-}
-
 try {
-    Add-Type -Path $PSScriptRoot/$loadPath/YamlDotNet.dll `
+    Add-Type -Path $PSScriptRoot/YamlDotNet.dll `
         -ErrorAction Ignore | Out-Null
 } catch {
     Write-Verbose $_
 }
 
 try {
-    Import-Module $PSScriptRoot/$loadPath/psyml.dll `
+    Import-Module $PSScriptRoot/psyml.dll `
         -ErrorAction Ignore | Out-Null
 } catch {
     Write-Verbose $_
